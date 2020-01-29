dc3ff00e60bb3a6ed4487ef0119337ded80bca31
mvonderosten <markvonderosten@gmail.com>
2020-01-29 23:30:41 +0000

    second Revision

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
 
 import "file-loader?name=[name].[ext]!../index.html";
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() 
-  g;
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse;
+  });
+  console.log("Help Me!");
+};
+
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["ate", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return who[whoIndx] + "" + what[whatIndx] + "" + when[whenIndx];
+};
