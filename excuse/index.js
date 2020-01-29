09baa5a61f361872ddf26c824e4da19f3e95bdde
mvonderosten <markvonderosten@gmail.com>
2020-01-29 16:36:08 +0000

    First Revision Upload

--- excuse/index.js
+++ excuse/index.js
@@ -1,4 +1,24 @@
-let who = ['the dog','my granma','his turtle','my bird'];
-let what = ['eat','pissed','crushed','broked'];
-let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+import "./excuse/index.scss";
 
+window.onload = () => {
+    
+  let randomNumber = Math.random() * 10;
+    document.querySelector('#btn').addEventListener("click", () =>  {
+    document.querySelector('#excuse').innerHTML = generateExcuse;  
+    });
+    console.log("Help Me!");
+
+};
+
+let generateExcuse = () => {
+
+    let who = ['the dog','my granma','his turtle','my bird'];
+    let what = ['ate','pissed','crushed','broked'];
+    let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+    let whoIndx = Math.floor(Math.random() * who.length;
+    let whatIndx = Math.floor(Math.random() * what.length;
+    let whenIndx = Math.floor(Math.random() * when.length;
+
+    return who[whoIndx] + '' + what[whatIndx] + '' + when[whenIndx];
+};
